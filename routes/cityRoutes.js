7e391d4f23351d71d45b6dde9dc6d97043d1591e
ealzayed <ealzayed@hotmail.com>
2022-12-15 10:07:48 +0300

    update 3

--- routes/cityRoutes.js
+++ routes/cityRoutes.js
@@ -5,11 +5,12 @@ const router = express.Router();
 const cityController = require('../controllers/cityController');
 
 
-router.get('/cities/create', cityController.city_create_get);
+
 
 router.get('/cities', cityController.city_index);
 
 router.post('/cities', cityController.city_create_post);
+router.get('/cities/create', cityController.city_create_get);
 
 router.get('/cities/:id', cityController.city_details);
 
